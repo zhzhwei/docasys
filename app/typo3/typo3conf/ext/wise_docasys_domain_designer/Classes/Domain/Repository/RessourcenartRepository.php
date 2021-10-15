1f09359ffe9aa00548f730837d886f83ec3b1eef
Marcel Susky <marcel.susky@tu-dresden.de>
2021-10-15 14:00:20 +0200

    Adds RessourcenartRepository to query database and create new instances

---
+++ app/typo3/typo3conf/ext/wise_docasys_domain_designer/Classes/Domain/Repository/RessourcenartRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace Wise\WiseDocasysDomainDesigner\Domain\Repository;
+
+/***
+ *
+ * This file is part of the "Domain Designer" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2021 Dr. Richard Braun <richard.braun@tu-dresden.de>, TU Dresden, Lehrstuhl für Wirtschaftsinformatik, insb. Systementwicklung
+ *
+ ***/
+
+/**
+ * The repository for Ressourcenarts
+ */
+class RessourcenartRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+    }
