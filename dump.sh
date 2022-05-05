6304bb23660d685abd78dba80c482cf2f8c7c4b6
Marcel Susky <marcel.susky@tu-dresden.de>
2022-05-05 10:45:43 +0200

    Updates mysqldump user to root user to fix privilege issues

--- dump.sh
+++ dump.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 
-docker exec -i docasys-db mysqldump -ucs-32_doca_u -pcs-32_doca_u cs-326244_doca > ./mysql/init/dump_$(date '+%Y-%m-%d').sql
+backup_dir=./mysql/init
+prev_backup=$(ls ${backup_dir}/*.sql | head -1)
+
+# save previous backup
+mv ${prev_backup} ${prev_backup}.bak
+
+# create database dump
+docker exec -i docasys-db mysqldump -uroot -proot cs-326244_doca > ${backup_dir}/dump_$(date '+%Y-%m-%d').sql
+
+# list backups
+ls -lh ${backup_dir}
